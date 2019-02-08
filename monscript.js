55345310e57dc230470a00aa941501394042a559
MarineTesolin <46956068+MarineTesolin@users.noreply.github.com>
2019-02-08 16:36:43 +0100

    Add files via upload

---
+++ monscript.js
@@ -0,0 +1,8 @@
+function mafonction() {
+  var change = document.getElementsById("texteepaule");
+  if (change.style.display === "none") {
+    change.style.display = "block";}
+   else {
+    change.style.display = "none";
+  }
+}
